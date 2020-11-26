fc7ebc8a04c537884be663295df2333ad8f79ec7
Borya <37692980+sescer@users.noreply.github.com>
2020-11-26 22:06:06 +0700

    Add files via upload

--- Task_1_2/src/main/java/ru/nsu/fit/oop/boryapatrushev/patternsearch/PatternSearch.java
+++ Task_1_2/src/main/java/ru/nsu/fit/oop/boryapatrushev/patternsearch/PatternSearch.java
@@ -1,11 +1,11 @@
 package ru.nsu.fit.oop.boryapatrushev.patternsearch;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class PatternSearch {
     /**
@@ -16,16 +16,16 @@ public class PatternSearch {
      * @return Collection with integers - indexes of occurrences
      */
     public static Collection<Integer> findAllOccurrences(String path, String substring) throws Exception {
-
+        if(substring.length() == 0){
+            throw new IllegalArgumentException();
+        }
         String halfstring;
-        String string = null;
+        String line = null;
         int len = substring.length() * 2;
         int scanned = 0;
         ArrayList<Integer> output;
-        try (
-             InputStream inputStream = PatternSearch.class.getClassLoader().getResourceAsStream(path)) {
-            assert inputStream != null;
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader( inputStream, StandardCharsets.UTF_8))) {
+        try (InputStream inputStream = PatternSearch.class.getClassLoader().getResourceAsStream(path);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream), StandardCharsets.UTF_8))) {
 
                 String pattern = new String(substring.getBytes(), StandardCharsets.UTF_8);
 
@@ -33,27 +33,26 @@ public class PatternSearch {
 
                 do {
                     if (scanned == 0) {
-                        string = readFixedLengthString(len, reader);
+                        line = readFixedLengthString(len, reader);
                     } else {
                         halfstring = readFixedLengthString(len / 2, reader);
-                        string = string.substring(len / 2);
-                        string += halfstring;
+                        line = line.substring(len / 2);
+                        line += halfstring;
                     }
 
-                    ArrayList<Integer> temRes = new ArrayList<>(findMatch(string, pattern));
+                    ArrayList<Integer> temRes = new ArrayList<>(findMatch(line, pattern));
                     for (int i = 0; i != temRes.size(); i++)
                         temRes.set(i, temRes.get(i) + scanned);
 
                     answer.addAll(temRes);
                     scanned += len / 2;
 
-                } while (!string.endsWith("\0"));
+                } while (!line.endsWith("\0"));
                 output = new ArrayList<>(answer);
+                inputStream.close();
+                reader.close();
                 output.sort(Integer::compareTo);
             }
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
         return output;
     }
 
@@ -66,12 +65,7 @@ public class PatternSearch {
      */
     private static String readFixedLengthString(int length, BufferedReader reader) throws IOException {
         char[] arr = new char[length];
-
-        try {
-            reader.read(arr, 0, length);
-        } catch (IOException e) {
-            throw new IOException("An error with I/O");
-        }
+        reader.read(arr, 0, length);
         return new String(arr);
     }
 
@@ -81,13 +75,13 @@ public class PatternSearch {
      * @param pattern Substring to be searched in text
      * @return ArrayList of integers with indexes of occurrences pattern in text
      */
-    private static ArrayList<Integer> findMatch(String text, String pattern) {
+    private static ArrayList<Integer> findMatch(String text, String pattern){
         ArrayList<Integer> results = new ArrayList<>();
-        String string = pattern + '#' + text;
-        int len = string.length();
+        String line = pattern + '#' + text;
+        int len = line.length();
         int[] Z = new int[len];
 
-        zFunc(string, Z);
+        zFunc(line, Z);
 
         for(int i = 0; i!= len; i++) {
             if (Z[i] == pattern.length()) {
@@ -100,19 +94,19 @@ public class PatternSearch {
     /**
      * The idea of Z-function is to concatenate pattern and text
      * Build the Z array for concatenated string.
-     * @param string pattern#text for prefix searching
+     * @param line pattern#text for prefix searching
      * @param Z results of z-function (empty in the beginning)
      */
 
-    private static void zFunc(String string, int[] Z) {
-        int len = string.length();
+    private static void zFunc(String line, int[] Z) {
+        int len = line.length();
         int L = 0, R = 0;
 
         for(int i = 0; i < len; i++) {
 
             if(i > R) {
                 R = L = i;
-                while(R < len && string.charAt(R - L) == string.charAt(R)) R++;
+                while(R < len && line.charAt(R - L) == line.charAt(R)) R++;
                 Z[i] = R - L;
                 R--;
             }
@@ -124,7 +118,7 @@ public class PatternSearch {
                 else
                 {
                     L = i;
-                    while(R < len && string.charAt(R - L) == string.charAt(R)) R++;
+                    while(R < len && line.charAt(R - L) == line.charAt(R)) R++;
                     Z[i] = R - L;
                     R--;
                 }
--- Task_1_2/src/test/java/ru/nsu/fit/oop/boryapatrushev/patternsearch/Tests.java
+++ Task_1_2/src/test/java/ru/nsu/fit/oop/boryapatrushev/patternsearch/Tests.java
@@ -1,22 +1,24 @@
 package ru.nsu.fit.oop.boryapatrushev.patternsearch;
 
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 
 public class Tests {
     @Test
     public void test_1() throws Exception {
 
-        Collection answer = PatternSearch.findAllOccurrences("test_1_1.txt", "пирог");
-        ArrayList<Integer> expected = new ArrayList<>();
+        Collection<Integer> answer = PatternSearch.findAllOccurrences("test_1_1.txt", "пирог");
+        List<Integer> expected = new ArrayList<>();
         expected.add(7);
         Assert.assertEquals(answer, expected);
 
@@ -28,8 +30,8 @@ public class Tests {
     @Test
     public void test_2() throws Exception {
 
-        Collection answer = PatternSearch.findAllOccurrences("test_2.txt", "java\r\nwant");
-        ArrayList<Integer> expected = new ArrayList<>();
+        Collection<Integer> answer = PatternSearch.findAllOccurrences("test_2.txt", "java\r\nwant");
+        List<Integer> expected = new ArrayList<>();
         expected.add(21);
         expected.add(38);
 
@@ -39,25 +41,59 @@ public class Tests {
     @Test
     public void test_3() throws Exception {
 
-        Collection answer = PatternSearch.findAllOccurrences("test_3.txt", "bruh");
-        ArrayList<Integer> expected = new ArrayList<>();
+        Collection<Integer> answer = PatternSearch.findAllOccurrences("test_3.txt", "java");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(21);
+        Assert.assertEquals(answer, expected);
+    }
+    @Test
+    public void test_4() throws Exception {
 
+        Collection<Integer> answer = PatternSearch.findAllOccurrences("test_4.txt", "abaf");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(382);
         Assert.assertEquals(answer, expected);
     }
 
     @Test
-    public void test_4() throws Exception {
-        File gen = new File("src/test/resources/test_4.txt");
-        BufferedWriter writer = new BufferedWriter(new FileWriter(gen));
-        for(long i = 0; i != 5000000L; i++) {
-            writer.write("ja");
-        }
+    public void test_5() throws Exception {
 
-        Collection answer = PatternSearch.findAllOccurrences(  "test_4.txt", "bruh");
-        ArrayList<Integer> expected = new ArrayList<>();
+        Collection<Integer> answer = PatternSearch.findAllOccurrences("test_5.txt", "bruh");
+        List<Integer> expected = new ArrayList<>();
+
         Assert.assertEquals(answer, expected);
+    }
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    @Test
+    public void test_6() throws Exception {
+        exception.expect(IllegalArgumentException.class);
+        PatternSearch.findAllOccurrences("test_6.txt", "");
+    }
 
-        gen.delete();
+    @Test
+    public void test_7() throws Exception {
+        String filePath = getFromResources("test_7.txt");
+        File gen = new File(filePath);
+        gen.createNewFile();
+        FileWriter writer= new FileWriter(filePath);
+        String str = "I fell for your magic, I tasted your skin " +
+                "And though this is tragic, at least I found the end ".repeat(42) +
+                "I witnessed your madness, you shed light on my sins ".repeat(42) +
+                "And if we share in this sadness, then where have you been? \n".repeat(42);
+        List<Integer> expected = new ArrayList<>();
+        for (long i = 0; i < ( 1024 * 1024 / str.length()); i++) {
+            writer.write(str);
+            expected.add((int)i * str.length());
+        }
+        writer.close();
+
+        Collection<Integer> answer = PatternSearch.findAllOccurrences(  "test_7.txt", "I fell for your magic,");
+        Assert.assertEquals(answer, expected);
+    }
+    private String getFromResources(String fileName) {
+        ClassLoader classLoader = PatternSearch.class.getClassLoader();
+        return Objects.requireNonNull(classLoader.getResource(fileName)).getPath();
     }
 
 }
